6afb5996f47435289e1332310e142dae9e9d693d
w2k8 <w2k8>
2022-02-04 14:39:03 +0100

    14.7.1

--- build_gitlab_docker.sh
+++ build_gitlab_docker.sh
@@ -2,3 +2,5 @@ source RELEASE
 wget ${DOWNLOAD_URL} -O gitlab.deb
 sudo docker build . -t ${RELEASE_PACKAGE}:${RELEASE_VERSION}
 sudo docker image save ${RELEASE_PACKAGE}:${RELEASE_VERSION} -o ${RELEASE_PACKAGE}_${RELEASE_VERSION}.docker
+sudo chmod 666 ${RELEASE_PACKAGE}_${RELEASE_VERSION}.docker
+ 
